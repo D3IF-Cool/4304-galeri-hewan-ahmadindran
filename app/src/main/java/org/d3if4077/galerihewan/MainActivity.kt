2aa0a50034db14c67cc0ee1070756bc84c2db23b
Ahmad Indra Nurfauzi <ahmadindran@student.telkomuniversity.ac.id>
2021-03-09 16:26:01 +0700

    Manambahkan view binding

--- app/src/main/java/org/d3if4077/galerihewan/MainActivity.kt
+++ app/src/main/java/org/d3if4077/galerihewan/MainActivity.kt
@@ -3,27 +3,32 @@ package org.d3if4077.galerihewan
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import org.d3if4077.galerihewan.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         Log.d("MainActivity", "Jumlah data: " + getData().size)
     }
 
     private fun getData(): List<Hewan> {
         return listOf(
-                Hewan("Angsa", "Cygnus olor"),
-                Hewan("Ayam", "Gallus gallus"),
-                Hewan("Bebek", "Cairina moschata"),
-                Hewan("Domba", "Ovis ammon"),
-                Hewan("Kalkun", "Meleagris gallopavo"),
-                Hewan("Kambing", "Capricoenis sumatrensis"),
-                Hewan("Kelinci", "Oryctolagus cuniculus"),
-                Hewan("Kerbau", "Bubalus bubalis"),
-                Hewan("Kuda", "Equus caballus"),
-                Hewan("Sapi", "Bos taurus")
+            Hewan("Angsa", "Cygnus olor"),
+            Hewan("Ayam", "Gallus gallus"),
+            Hewan("Bebek", "Cairina moschata"),
+            Hewan("Domba", "Ovis ammon"),
+            Hewan("Kalkun", "Meleagris gallopavo"),
+            Hewan("Kambing", "Capricoenis sumatrensis"),
+            Hewan("Kelinci", "Oryctolagus cuniculus"),
+            Hewan("Kerbau", "Bubalus bubalis"),
+            Hewan("Kuda", "Equus caballus"),
+            Hewan("Sapi", "Bos taurus")
         )
     }
 }
