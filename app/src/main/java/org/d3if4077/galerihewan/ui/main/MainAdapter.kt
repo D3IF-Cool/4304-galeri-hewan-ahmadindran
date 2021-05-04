cb628178e3d2bad3e0a188768764837099133482
Ahmad Indra Nurfauzi <ahmadindran@student.telkomuniversity.ac.id>
2021-05-04 13:15:02 +0700

    Menampilkan gambar dari internet

--- app/src/main/java/org/d3if4077/galerihewan/ui/main/MainAdapter.kt
+++ app/src/main/java/org/d3if4077/galerihewan/ui/main/MainAdapter.kt
@@ -3,8 +3,11 @@ package org.d3if4077.galerihewan.ui.main
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import org.d3if4077.galerihewan.R
 import org.d3if4077.galerihewan.databinding.ListItemBinding
 import org.d3if4077.galerihewan.model.Hewan
+import org.d3if4077.galerihewan.network.HewanApi
 
 class MainAdapter: RecyclerView.Adapter<MainAdapter.ViewHolder>() {
 
@@ -22,7 +25,10 @@ class MainAdapter: RecyclerView.Adapter<MainAdapter.ViewHolder>() {
         fun bind(hewan: Hewan) = with(binding){
             namaTextView.text = hewan.nama
             latinTextView.text = hewan.namaLatin
-            imageView.setImageResource(hewan.imageResId)
+            Glide.with(imageView.context)
+                .load(HewanApi.getHewanUrl(hewan.imageId))
+                .error(R.drawable.ic_baseline_broken_image_24)
+                .into(imageView)
         }
     }
 
