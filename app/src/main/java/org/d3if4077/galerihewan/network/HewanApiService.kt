167549c4de81c2fd0b0772e548b545af55e9b956
Ahmad Indra Nurfauzi <ahmadindran@student.telkomuniversity.ac.id>
2021-05-04 13:05:37 +0700

    Memproses data internet sebagai JSON

--- app/src/main/java/org/d3if4077/galerihewan/network/HewanApiService.kt
+++ app/src/main/java/org/d3if4077/galerihewan/network/HewanApiService.kt
@@ -1,20 +1,24 @@
 package org.d3if4077.galerihewan.network
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import org.d3if4077.galerihewan.model.Hewan
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 
 private const val BASE_URL = "https://dif.indraazimi.com/"
 
-private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
-    .baseUrl(BASE_URL)
-    .build()
+private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+
+private val retrofit =
+    Retrofit.Builder().addConverterFactory(MoshiConverterFactory.create(moshi)).baseUrl(BASE_URL)
+        .build()
 
 interface HewanApiService {
     @GET("listhewan.json")
-    suspend fun getHewan(): String
+    suspend fun getHewan(): List<Hewan>
 }
 
 object HewanApi {
--- app/src/main/java/org/d3if4077/galerihewan/ui/main/MainViewModel.kt
+++ app/src/main/java/org/d3if4077/galerihewan/ui/main/MainViewModel.kt
@@ -6,7 +6,6 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
-import org.d3if4077.galerihewan.R
 import org.d3if4077.galerihewan.model.Hewan
 import org.d3if4077.galerihewan.network.HewanApi
 
@@ -15,34 +14,17 @@ class MainViewModel : ViewModel() {
     private var data = MutableLiveData<List<Hewan>>()
 
     init {
-        data.value = initData()
         retrieveData()
     }
 
     private fun retrieveData() {
         viewModelScope.launch {
             try {
-                val result = HewanApi.service.getHewan()
-                Log.d("MainViewModel", "Success: $result")
+                data.value = HewanApi.service.getHewan()
             } catch (e: Exception) {
                 Log.d("MainViewModel", "Failure: ${e.message}")
             }
         }
-    }
-
-    private fun initData(): List<Hewan> {
-        return listOf(
-            Hewan("Angsa", "Cygnus olor", R.drawable.angsa),
-            Hewan("Ayam", "Gallus gallus", R.drawable.ayam),
-            Hewan("Bebek", "Cairina moschata", R.drawable.bebek),
-            Hewan("Domba", "Ovis ammon", R.drawable.domba),
-            Hewan("Kalkun", "Meleagris gallopavo", R.drawable.kalkun),
-            Hewan("Kambing", "Capricornis sumatrensis", R.drawable.kambing),
-            Hewan("Kelinci", "Oryctolagus cuniculus", R.drawable.kelinci),
-            Hewan("Kerbau", "Bubalus bubalis", R.drawable.kerbau),
-            Hewan("Kuda", "Equus caballus", R.drawable.kuda),
-            Hewan("Sapi", "Bos taurus", R.drawable.sapi)
-        )
     }
 
     fun getData(): LiveData<List<Hewan>> = data
